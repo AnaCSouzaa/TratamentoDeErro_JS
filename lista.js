6b45e97a2b4cb933e33d48d4937072bb579d872d
Ana Cecília Souza <anaceciams@gmail.com>
2022-05-06 08:47:28 -0300

    exemplo try catch

--- lista.js
+++ lista.js
@@ -1,3 +1,16 @@
+function DeleteItem(){
+    console.log(this.parentElement)
+    this.parentElement.remove()
+}
+
+function EditItem(){
+    item_clicado = this.parentElement
+    valor_lista = item_clicado.textContent.replace("DeleteEdit","")
+    document.querySelector('[data-form-input]').value = valor_lista;
+    document.querySelector('[button-input]').classList.add("btn", "btn-danger", "d-none")
+    document.querySelector('[button-edit]').classList.remove("d-none")
+}
+
 function CriarBotaoDelete(){
     const botao_delete =  document.createElement('button')
     botao_delete.classList.add("btn", "btn-danger")
@@ -7,61 +20,42 @@ function CriarBotaoDelete(){
 
     return botao_delete
 }
-function DeleteItem(){
-    console.log(this.parentElement)
-    this.parentElement.remove()
-}
-
 
 function CriarBotaoEdit(){
     const botao_edit =  document.createElement('button')
-    botao_edit.classList.add("btn", "btn-success")
+    botao_edit.classList.add("btn", "btn-dark")
     botao_edit.innerHTML = "Edit"
     botao_edit.type = "button"
     botao_edit.addEventListener("click", EditItem);
 
     return botao_edit
-} 
-function EditItem(){
-    const novo_item_lista = document.createElement('input');
-    const FatherElement = this.parentElement
-    FatherElement.innerHTML = '';
-    FatherElement.appendChild(novo_item_lista)
-    FatherElement.appendChild(CriarBotaoEdit())
-    FatherElement.appendChild(CriarBotaoSalvar())
 }
-
-
-function CriarBotaoSalvar(){
-    const botao_salvar =  document.createElement('button')
-    botao_salvar.classList.add("btn", "btn-secondary")
-    botao_salvar.innerHTML = "Salvar"
-    botao_salvar.type = "button"
-    botao_salvar.addEventListener("click", SalvarItem);
-
-    return botao_salvar
-} 
-function SalvarItem(){
-    const input = this.parentElement.children[0] 
-    lista = this.parentElement
-    this.parentElement.innerHTML = input.value
-    lista.appendChild(CriarBotaoDelete())
-    lista.appendChild(CriarBotaoEdit())   
-}
-
 
 function Submit(){
-    const lista = document.querySelector('[data-task]')
-    const valor = document.querySelector('[data-form-input]')
-
+    const lista = document.querySelector('[data-task]')  
+    try{
+        valor = document.querySelector('[data-form-input]')  
+    }
+    catch{
+        valor = ""
+        console.log(error)
+    }
     const novo_item_lista = document.createElement("li")
     novo_item_lista.className = "list-group-item";
-
-    novo_item_lista.innerHTML = valor.value
- 
+    novo_item_lista.innerHTML = valor
     novo_item_lista.appendChild(CriarBotaoDelete())
     novo_item_lista.appendChild(CriarBotaoEdit())
+
     lista.appendChild (novo_item_lista)
 
     document.getElementById("item").value = ""
+}
+
+function Edit(){
+    item_clicado.innerText = document.querySelector('[data-form-input]').value
+    item_clicado.appendChild(CriarBotaoDelete())
+    item_clicado.appendChild(CriarBotaoEdit())
+
+    document.querySelector('[button-edit]').className = "btn btn-dark d-none"
+    document.querySelector('[button-input]').classList.remove("d-none")
 }
